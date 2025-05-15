667f5f06491a41d312a32b2996bba11bba5c6ec8
Ilgar Ilyasov <garyasov@gmail.com>
2025-05-15 12:38:16 -0400

    Initial Commit

---
+++ Pokeapi/PokeapiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeapiApp.swift
+//  Pokeapi
+//
+//  Created by David on 2025-05-15.
+//
+
+import SwiftUI
+
+@main
+struct PokeapiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
